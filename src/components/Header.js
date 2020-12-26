456bcd790759434e632baa59a3a07b8947fbe1b3
agentraghav <raghvendrarathore2@gmail.com>
2020-12-26 12:31:35 +0530

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/header.css'
+function Header() {
+    return (
+        <div className="jumbotron">
+            <h1 >CV Application</h1>
+        </div>
+    )
+}
+
+export default Header
